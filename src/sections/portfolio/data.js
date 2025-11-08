8b71d78b4164794093152c966426200a8379894c
Maria Lima <mariaclimany@gmail.com>
2025-11-08 07:48:05 -0500

    change order of items in portfolio

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -10,38 +10,39 @@ import Image9 from '../../assets/youd-look-good.webp'
 import Image10 from '../../assets/press-big-bucks.webp'
 import Image11 from '../../assets/spin-wheel.webp'
 import Image12 from '../../assets/mariaclima.webp'
-import Image13 from '../../assets/to-do-list.webp'
-import Image14 from '../../assets/imperium-platform.webp'
-import Image15 from '../../assets/quantumstat.webp'
+import Image13 from '../../assets/ampyra.webp'
+import Image14 from '../../assets/ampyra-hcp.webp'
+import Image15 from '../../assets/inbrija.webp'
+import Image16 from '../../assets/xeomin.webp'
+import Image17 from '../../assets/merz-aesthetics.webp'
+import Image18 from '../../assets/ultherapy.webp'
+import Image19 from '../../assets/live-local-music.webp'
+import Image20 from '../../assets/to-do-list.webp'
+import Image21 from '../../assets/imperium-platform.webp'
+import Image22 from '../../assets/quantumstat.webp'
 //import Image16 from '../../assets/twin-lions-global-health.webp'
-import Image16 from '../../assets/salih-salon.webp'
-import Image17 from '../../assets/armelle-for-kids.webp'
-import Image18 from '../../assets/mclinteractive.webp'
+import Image23 from '../../assets/salih-salon.webp'
+import Image24 from '../../assets/armelle-for-kids.webp'
+import Image25 from '../../assets/mclinteractive.webp'
 // import Image19 from '../../assets/foxbusiness.webp'
-import Image19 from '../../assets/pionline.webp'
-import Image20 from '../../assets/map-leaflet.webp'
-import Image21 from '../../assets/countdown.webp'
-import Image22 from '../../assets/responsive-carousel.webp'
-import Image23 from '../../assets/ada-slider.webp'
-import Image24 from '../../assets/mobile-slider.webp'
-import Image25 from '../../assets/slider-win-your-dreams.webp'
-import Image26 from '../../assets/tic-tac-toe.webp'
-import Image27 from '../../assets/rate-calculator.gif'
-import Image28 from '../../assets/pch-payments.webp'
-import Image29 from '../../assets/nytimes-news-app.webp'
-import Image30 from '../../assets/accordion-app.webp'
-import Image31 from '../../assets/show-more-app.webp'
-import Image32 from '../../assets/tip-calculator-app.webp'
-import Image33 from '../../assets/currency-converter-app.webp'
-import Image34 from '../../assets/recipe-finder-app.webp'
-import Image35 from '../../assets/ampyra.webp'
-import Image36 from '../../assets/ampyra-hcp.webp'
-import Image37 from '../../assets/inbrija.webp'
-import Image38 from '../../assets/xeomin.webp'
-import Image39 from '../../assets/merz-aesthetics.webp'
-import Image40 from '../../assets/ultherapy.webp'
-import Image41 from '../../assets/live-local-music.webp'
-import Image42 from '../../assets/wordle.webp'
+import Image26 from '../../assets/pionline.webp'
+import Image27 from '../../assets/map-leaflet.webp'
+import Image28 from '../../assets/countdown.webp'
+import Image29 from '../../assets/responsive-carousel.webp'
+import Image30 from '../../assets/ada-slider.webp'
+import Image31 from '../../assets/mobile-slider.webp'
+import Image32 from '../../assets/slider-win-your-dreams.webp'
+import Image33 from '../../assets/tic-tac-toe.webp'
+import Image34 from '../../assets/rate-calculator.gif'
+import Image35 from '../../assets/pch-payments.webp'
+import Image36 from '../../assets/nytimes-news-app.webp'
+import Image37 from '../../assets/recipe-finder-app.webp'
+import Image38 from '../../assets/tip-calculator-app.webp'
+import Image39 from '../../assets/currency-converter-app.webp'
+import Image40 from '../../assets/wordle.webp'
+import Image41 from '../../assets/accordion-app.webp'
+import Image42 from '../../assets/show-more-app.webp'
+
 
 const data = [
   {
@@ -160,24 +161,87 @@ const data = [
     id: 13,
     category: 'Websites',
     image: Image13,
-    title: 'Plus2SD - To-Do List',
-    desc: 'Built with PHP, MySQL, JavaScript, CSS, HTML. Demo available upon request.',
-    demo: 'https://plus2sd.com/',
+    title: 'Ampyra - Healthcare',
+    desc: 'Rebuilt with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML, Vite.',
+    demo: 'https://ampyra.com',
     github: '',
   },
   {
     id: 14,
     category: 'Websites',
     image: Image14,
-    title: 'Imperium Platform - Machine Learning',
-    desc: 'Built frontend with JavaScript, CSS, HTML.',
-    demo: '',
+    title: 'Ampyra HCP - Healthcare',
+    desc: 'Rebuilt with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML, Vite.',
+    demo: 'https://ampyra-hcp.com',
     github: '',
   },
   {
     id: 15,
     category: 'Websites',
     image: Image15,
+    title: 'Inbrija - Healthcare',
+    desc: 'Maintained and enhanced with React, SASS, JavaScript, CSS, HTML.',
+    demo: 'https://inbrija.com',
+    github: '',
+  },
+  {
+    id: 16,
+    category: 'Websites',
+    image: Image16,
+    title: 'Xeomin - Healthcare',
+    desc: 'Maintained and enhanced with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML.',
+    demo: 'https://xeomin.com',
+    github: '',
+  },
+  {
+    id: 17,
+    category: 'Websites',
+    image: Image17,
+    title: 'Merz Aesthetics - Healthcare',
+    desc: 'Mantained and enhanced with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML.',
+    demo: 'https://merzaesthetics.com',
+    github: '',
+  },
+  {
+    id: 18,
+    category: 'Websites',
+    image: Image18,
+    title: 'Ultherapy - Healthcare',
+    desc: 'Rebuilt with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML, Gulp.',
+    demo: 'https://ultherapy.com',
+    github: '',
+  },
+  {
+    id: 19,
+    category: 'Websites',
+    image: Image19,
+    title: 'Live Local Music - Entertainment',
+    desc: 'Built with React, JavaScript, CSS, HTML, Vite, Firebase/Firestore.',
+    demo: 'https://livelocal.music',
+    github: '',
+  },
+  {
+    id: 20,
+    category: 'Websites',
+    image: Image20,
+    title: 'Plus2SD - To-Do List',
+    desc: 'Built with PHP, MySQL, JavaScript, CSS, HTML. Demo available upon request.',
+    demo: 'https://plus2sd.com/',
+    github: '',
+  },
+  {
+    id: 21,
+    category: 'Websites',
+    image: Image21,
+    title: 'Imperium Platform - Machine Learning',
+    desc: 'Built frontend with JavaScript, CSS, HTML.',
+    demo: '',
+    github: '',
+  },
+  {
+    id: 22,
+    category: 'Websites',
+    image: Image22,
     title: 'Quantum Stat - Machine Learning',
     desc: 'Built with Python, Django, JavaScript, CSS, HTML.',
     demo: '',
@@ -193,27 +257,27 @@ const data = [
   //   github: '',
   // },
   {
-    id: 16,
+    id: 23,
     category: 'Websites',
-    image: Image16,
+    image: Image23,
     title: 'Salih Salon - Beauty',
     desc: 'Built with WordPress, JavaScript, CSS, HTML.',
     demo: 'https://salihsalon.com',
     github: '',
   },
   {
-    id: 17,
+    id: 24,
     category: 'Websites',
-    image: Image17,
+    image: Image24,
     title: 'Armelle for Kids - Education',
     desc: 'Built with WordPress, JavaScript, CSS, HTML.',
     demo: 'https://armelleforkids.com',
     github: '',
   },
   {
-    id: 18,
+    id: 25,
     category: 'Websites',
-    image: Image18,
+    image: Image25,
     title: 'MCL Interactive - Web Development',
     desc: 'Built with WordPress, JavaScript, CSS, HTML.',
     demo: 'https://mclinteractive.com',
@@ -229,220 +293,157 @@ const data = [
   //   github: '',
   // },
   {
-    id: 19,
+    id: 26,
     category: 'Websites',
-    image: Image19,
+    image: Image26,
     title: 'Pensions & Investments - Financial News',
     desc: 'Maintained and enhanced the frontend with JavaScript, jQuery, CSS, HTML.',
     demo: 'https://pionline.com',
     github: '',
   },
   {
-    id: 20,
+    id: 27,
     category: 'UI Components',
-    image: Image20,
+    image: Image27,
     title: 'Leaflet/OpenStreetMap Map',
     desc: 'Built with Vite, JavaScript (ES Modules), Leaflet/OpenStreetMap. Added Cookie Consent Open Source Code by Orest Bida.',
     demo: 'https://mclima.github.io/leaflet/',
     github: 'https://github.com/mclima/leaflet',
   },
   {
-    id: 21,
+    id: 28,
     category: 'UI Components',
-    image: Image21,
+    image: Image28,
     title: 'Countdown',
     desc: 'Built with JavaScript.',
     demo: 'https://codepen.io/mlima2020/pen/poWdXxw',
     github: '',
   },
   {
-    id: 22,
+    id: 29,
     category: 'UI Components',
-    image: Image22,
+    image: Image29,
     title: 'Responsive Carousel',
     desc: 'Built with JavaScript.',
     demo: 'https://codepen.io/mlima2020/pen/raBXvbb',
     github: '',
   },
   {
-    id: 23,
+    id: 30,
     category: 'UI Components',
-    image: Image23,
+    image: Image30,
     title: 'ADA Slider',
     desc: 'Built with JavaScript.',
     demo: 'https://codepen.io/mlima2020/pen/wvQapZQ',
     github: '',
   },
   {
-    id: 24,
+    id: 31,
     category: 'UI Components',
-    image: Image24,
+    image: Image31,
     title: 'Slider w/ Arrows and Dots',
     desc: 'Built with JavaScript.',
     demo: 'https://codepen.io/mlima2020/pen/gONWGob',
     github: '',
   },
   {
-    id: 25,
+    id: 32,
     category: 'UI Components',
-    image: Image25,
+    image: Image32,
     title: 'Slider',
     desc: 'Built with JavaScript, GSAP.',
     demo: 'https://codepen.io/mlima2020/pen/QWRwXgG',
     github: '',
   },
   {
-    id: 26,
+    id: 33,
     category: 'UI Components',
-    image: Image26,
+    image: Image33,
     title: 'Tic Tac Toe',
     desc: 'Built with jQuery.',
     demo: 'https://codepen.io/mlima2020/pen/zYEPXKy',
     github: '',
   },
   {
-    id: 27,
+    id: 34,
     category: 'UI Components',
-    image: Image27,
+    image: Image34,
     title: 'Rate Calculator',
     desc: 'Built with JavaScript.',
     demo: 'https://websystemsview.com/sv/webdevelopment/javascript/javascript.html#rateForm',
     github: 'https://github.com/mclima/rate-calculator',
   },
   {
-    id: 28,
+    id: 35,
     category: 'UI Components',
-    image: Image28,
+    image: Image35,
     title: "PCH Customers' Weekly Prize Payments",
     desc: 'Built with JavaScript.',
     demo: 'https://mariaclima.com/pch/calculations/pch-prize-payments.html',
     github: 'https://github.com/mclima/weekly-prize-payments',
   },
   {
-    id: 29,
+    id: 36,
     category: 'React',
-    image: Image29,
+    image: Image36,
     title: 'NYTimes Stories - Fetch API',
     desc: 'Built with Preact and Vite.',
     demo: 'https://mclima.github.io/news-app/',
     github: 'https://github.com/mclima/news-app',
   },
   {
-    id: 30,
+    id: 37,
     category: 'React',
-    image: Image30,
-    title: 'JS Q&As - Accordion',
-    desc: 'Built with React.',
-    demo: 'https://mclima.github.io/accordion-app/',
-    github: 'https://github.com/mclima/accordion-app',
-  },
-  {
-    id: 31,
-    category: 'React',
-    image: Image31,
-    title: 'Show More',
-    desc: 'Built with React.',
-    demo: 'https://mclima.github.io/show-more-app/',
-    github: 'https://github.com/mclima/show-more-app',
-  },
-  {
-    id: 32,
-    category: 'React',
-    image: Image32,
-    title: 'Tip Calculator',
-    desc: 'Built with React.',
-    demo: 'https://mclima.github.io/tip-calculator-app/',
-    github: 'https://github.com/mclima/tip-calculator-app',
-  },
-  {
-    id: 33,
-    category: 'React',
-    image: Image33,
-    title: 'Currency Converter - Fetch API',
-    desc: 'Built with React.',
-    demo: 'https://mclima.github.io/currency-converter-app/',
-    github: 'https://github.com/mclima/currency-converter-app',
-  },
-  {
-    id: 34,
-    category: 'React',
-    image: Image34,
+    image: Image37,
     title: 'Recipe Finder App - Fetch API',
     desc: 'Built with React, Bootstrap, SASS, Vite.',
     demo: 'https://mclima.github.io/recipe-finder-app/',
     github: 'https://github.com/mclima/recipe-finder-app',
   },
   {
-    id: 35,
-    category: 'Websites',
-    image: Image35,
-    title: 'Ampyra - Healthcare',
-    desc: 'Rebuilt with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML, Vite.',
-    demo: 'https://ampyra.com',
-    github: '',
-  },
-  {
-    id: 36,
-    category: 'Websites',
-    image: Image36,
-    title: 'Ampyra HCP - Healthcare',
-    desc: 'Rebuilt with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML, Vite.',
-    demo: 'https://ampyra-hcp.com',
-    github: '',
-  },
-  {
-    id: 37,
-    category: 'Websites',
-    image: Image37,
-    title: 'Inbrija - Healthcare',
-    desc: 'Maintained and enhanced with React, SASS, JavaScript, CSS, HTML.',
-    demo: 'https://inbrija.com',
-    github: '',
-  },
-  {
     id: 38,
-    category: 'Websites',
+    category: 'React',
     image: Image38,
-    title: 'Xeomin - Healthcare',
-    desc: 'Maintained and enhanced with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML.',
-    demo: 'https://xeomin.com',
-    github: '',
+    title: 'Tip Calculator',
+    desc: 'Built with React.',
+    demo: 'https://mclima.github.io/tip-calculator-app/',
+    github: 'https://github.com/mclima/tip-calculator-app',
   },
   {
     id: 39,
-    category: 'Websites',
+    category: 'React',
     image: Image39,
-    title: 'Merz Aesthetics - Healthcare',
-    desc: 'Mantained and enhanced with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML.',
-    demo: 'https://merzaesthetics.com',
-    github: '',
+    title: 'Currency Converter - Fetch API',
+    desc: 'Built with React.',
+    demo: 'https://mclima.github.io/currency-converter-app/',
+    github: 'https://github.com/mclima/currency-converter-app',
   },
   {
     id: 40,
-    category: 'Websites',
+    category: 'React',
     image: Image40,
-    title: 'Ultherapy - Healthcare',
-    desc: 'Rebuilt with WordPress (Timber/Twig), PHP, SASS, JavaScript, CSS, HTML, Gulp.',
-    demo: 'https://ultherapy.com',
+    title: 'Wordle Game',
+    desc: 'Built with React, CSS, HTML, Vite.',
+    demo: 'https://mclima.github.io/wordle/',
     github: '',
   },
   {
     id: 41,
-    category: 'Websites',
+    category: 'React',
     image: Image41,
-    title: 'Live Local Music - Entertainment',
-    desc: 'Built with React, JavaScript, CSS, HTML, Vite, Firebase/Firestore.',
-    demo: 'https://livelocal.music',
-    github: '',
+    title: 'JS Q&As - Accordion',
+    desc: 'Built with React.',
+    demo: 'https://mclima.github.io/accordion-app/',
+    github: 'https://github.com/mclima/accordion-app',
   },
   {
     id: 42,
     category: 'React',
     image: Image42,
-    title: 'Wordle Game',
-    desc: 'Built with React, CSS, HTML, Vite.',
-    demo: 'https://mclima.github.io/wordle/',
-    github: '',
+    title: 'Show More',
+    desc: 'Built with React.',
+    demo: 'https://mclima.github.io/show-more-app/',
+    github: 'https://github.com/mclima/show-more-app',
   },
 ]
 
